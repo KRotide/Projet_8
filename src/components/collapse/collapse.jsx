6b71f84f798099fecb9887b4dd50d66f074c9a31
KRotide <Cheepster38@hotmail.fr>
2023-07-13 19:51:52 +0200

    Avancement de la page "à propos"

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -1,27 +1,32 @@
 import "./collapse.scss";
-import aboutDatas from "../../datas/about.json"
-import ArrowUp from "../../assets/Arrow-up.png";
-import ArrowDown from "../../assets/Arrow-down.png";
-import { useState } from "react";
+import arrow from "../../assets/arrow.png";
+import React, { useState } from "react";
 
-export default function Collapse() {
-    const [toggle, setToggle] = useState(false);
+export default function Collapse(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openContent = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
-        <div className="collapse">
-            {aboutDatas.map((data) => (
-                <div key={data.id} className="collapse__dropdown">
-                    <h3 className="collapse__dropdown__title" onClick={() => setToggle(!toggle)}>
-                        {data.aboutTitle}
-                        <img className="collapse__dropdown__icon" src={toggle ? ArrowDown : ArrowUp} alt="voir le contenu" />
-                    </h3>
-                    {toggle && (
-                        <div className="collapse__dropdown__txt">
-                            {data.aboutText}
-                        </div>
-                    )}
-                </div>
-            ))}
+        <div className={`collapse ${isOpen ? 'collapse__dropdown--open' : ''}`}>
+            <div className="collapse__dropdown">
+                <h3 className="collapse__dropdown__title">
+                    {props.title}
+                    <img
+                        className={`collapse__dropdown__arrow ${isOpen ? 'collapse__dropdown__arrow--down' : 'collapse__dropdown__arrow--up'}`}
+                        onClick={openContent}
+                        src={arrow}
+                        alt="voir le contenu"
+                    />
+                </h3>
+                {isOpen &&
+                    <div className="collapse__dropdown__txt">
+                        {props.content}
+                    </div>
+                }
+            </div>
         </div>
-    )
+    );
 }
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,6 +1,8 @@
 import "./about.scss";
+import "../../components/collapse/collapse.scss"
 import Header from "../../components/header/header";
 import Banner from "../../components/banner/banner";
+import DatasAbout from "../../datas/about.json";
 import Collapse from "../../components/collapse/collapse";
 import Footer from "../../components/footer/footer";
 
@@ -9,7 +11,14 @@ export default function About() {
         <div className="about">
             <Header />
             <Banner />
-            <Collapse />
+            {DatasAbout.map((data) => (
+                <Collapse
+                    key={data.id}
+                    id={data.id}
+                    title={data.aboutTitle}
+                    content={data.aboutText}
+                />
+            ))}
             <Footer />
         </div>
     )
